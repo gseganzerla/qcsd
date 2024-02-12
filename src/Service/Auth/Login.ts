bf367cb98f6aa68633922dbfe3b256cbc23f7165
gseganzerla <g.seganzerla@gmail.com>
2024-02-12 22:53:06 +0000

    sla

--- src/Service/Auth/Login.ts
+++ src/Service/Auth/Login.ts
@@ -18,7 +18,7 @@ export class LoginService {
             grant_type: "authorization_code",
             code,
             redirect_uri: process.env.REDIRECT_URI,
-            code_verifier: '7KYsbXdINp4aeobAsSIjsfRQRoL2Pm8U7GglXpRrcElIBUxZQMlsBXTEyrHaQM2r',
+            code_verifier: process.env.CODE_VERIFIER,
 
         }, {
             headers: {
@@ -53,3 +53,10 @@ export class LoginService {
 
 
 }
+
+const l = new LoginService()
+
+// l.getCode()
+l.login('AQAn9dE_dGlv3R3edFPwXu9PgqwzOqDbt_gCgNGvUDqYPAtLSlCr_yOl6dCpfWfIZ4MsZ3hBMdMPMIH7LIoERGSu-GIA4epNUXqVqGVN6DlL9Vk0dIfHnkVbpsyycDwg9tcBkpTT8mBnqOmP5jUopopnHYEf-OCAfMS5-4q-1pR1Ac_IfD9NECkNzzaoWdAMbLXTCo72XxzJK-hoq7UuKIBkrWu32Kanp_YLDYariqiFXwtEfEGOS1wgTwmgrMEGJqf5hHz7Yuj8DHDbDePZbOcf5I3xrjuxBDwayrQqtaVEUUZo_N_QA_kGzUjM_DpdcZMb6cVJHTDh6c7JFmdIJ2M')
+// l.login('AQAAKJ2GjWyX4ia5mKgWeivyolkGH0E8Jqdp9CTCV_EAsZFXoS1-X2knpkCRRKyGwxgaWVHcClR2_9HwJFiw41cdqupMD6jdtyzR2vUQTcS3Ybaj79hAC-l9_NyJ-yw8OvoWh0gIlGXVzsc-cIYGQQLA-oECuLFd6J75jWw34mckFbhp9nsW5DLviN8J1EfEOqIbEt1Rv__PBwk_YZeqggnlr-L_F5v_zviIbeUEefmCVLMzRZQMLvyuAXi0OpqNqH6-t_PIlmFeUCymy-PAuWwcHTpNtxdoEbGGw9PRm6caxFx6hLHVq1tsCFvi4cu2JZk173FUvQFYzFIL4dMHS74')
+// l.login('AQBpv08EhkNDi0EI16kMAof2t0imPWQtyYK_Gb7Ju4GTPuLZdMn2kBFmZ-Tdasf3MNVojPX9BETcRQlKtyB_oGxSSoykTvc8Wbqd8JjBp29FPg07462FHU7otxmCnHMmNBDl_s46Kv5cDRPcdmB8leexLqIYDUachnjMnx4Kqcc56-gQMRCoygBqfVOrDSyDZn884k9wbHYEUCj5_K6K3nix920XAAf1HID-RlaP3co5XKrCEb2eepZLPvMaILcw-eUs2-tqDxcgtJikR0F9hGd5uYnNrceHRA6MxctT5feEqiCvzvq3yfIZG67h7bEPUIpFd03pdcDOEBdfXid9pOM')
